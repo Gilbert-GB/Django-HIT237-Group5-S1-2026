d9d88bc12fefb2eaf7b1fb361f562c071b800a8c
Nawshin-nawar <nawshintanisha74@gmail.com>
2026-04-07 15:55:15 +0930

    Fixed CSS styling and added search filter UI with region and category dropdowns

--- youthjustice_app/views.py
+++ youthjustice_app/views.py
@@ -4,11 +4,19 @@ from .models import Program
 
 def home(request):
     search_query = request.GET.get("search", "")
+    selected_region = request.GET.get("region", "")
+    selected_category = request.GET.get("category", "")
+
+    programs = Program.objects.all()
 
     if search_query:
-        programs = Program.objects.filter(name__icontains=search_query)
-    else:
-        programs = Program.objects.all()
+        programs = programs.filter(name__icontains=search_query)
+
+    if selected_region:
+        programs = programs.filter(region=selected_region)
+
+    if selected_category:
+        programs = programs.filter(category=selected_category)
 
     featured_programs = Program.objects.filter(is_featured=True)
 
@@ -16,6 +24,10 @@ def home(request):
         "programs": programs,
         "featured_programs": featured_programs,
         "search_query": search_query,
+        "selected_region": selected_region,
+        "selected_category": selected_category,
+        "region_choices": Program.REGION_CHOICES,
+        "category_choices": Program.CATEGORY_CHOICES,
     }
 
-    return render(request, "home.html", context) 
+    return render(request, "home.html", context)
