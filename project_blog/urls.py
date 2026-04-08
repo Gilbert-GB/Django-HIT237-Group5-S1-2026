0e1de77e508db763b19301ef3494459b062dbe84
Nawshin-nawar <nawshintanisha74@gmail.com>
2026-04-08 18:20:47 +0930

    Separated home and programs pages and updated full template structure

--- project_blog/urls.py
+++ project_blog/urls.py
@@ -5,6 +5,7 @@ from youthjustice_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("programs/", views.programs, name="programs"),
     path("program/<int:program_id>/", views.program_detail, name="program_detail"),
     path("about/", views.about, name="about"),
 ]
