8f5fdc2d9d89aa7702a1eb2b4ba73615a998ddd7
Mahathir383874 <mahathirtaief25@gmail.com>
2026-04-09 21:13:32 +0600

    Register CrimeData and EngagementData models in the admin interface

--- youthjustice_app/admin.py
+++ youthjustice_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Program
+from .models import Program, CrimeData, EngagementData
 
 # Models are being registered here
 class ProgramAdmin(admin.ModelAdmin):
@@ -7,4 +7,19 @@ class ProgramAdmin(admin.ModelAdmin):
     list_filter = ("region", "category")
     search_fields = ("name", "organisation")
 
-admin.site.register(Program, ProgramAdmin) 
+admin.site.register(Program, ProgramAdmin)
+
+
+class CrimeDataAdmin(admin.ModelAdmin):
+    list_display = ("region", "offence_category", "year", "month", "count")
+    list_filter = ("region", "year", "offence_category")
+    search_fields = ("offence_category", "offence_type", "region")
+
+admin.site.register(CrimeData, CrimeDataAdmin)
+
+
+class EngagementDataAdmin(admin.ModelAdmin):
+    list_display = ("year", "sex", "indigenous_status", "value_nt", "value_national")
+    list_filter = ("year", "sex", "indigenous_status")
+
+admin.site.register(EngagementData, EngagementDataAdmin)
