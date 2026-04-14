245e881d4f1ca97f3d5f70cb785dd70030bdbc8e
Gilbertofer Tanoto <gilbertofer.tanoto01@gmail.com>
2026-04-14 14:16:59 +0930

    added DashboardView in the urls

--- youthjustice_app/urls.py
+++ youthjustice_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from youthjustice_app import views
+from youthjustice_app.views import DashboardView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
