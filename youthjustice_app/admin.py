cb56d2c82d021288aae67064caea395b86c79486
M-Ahmad-T <ch.muhammad.ahmad@gmail.com>
2026-04-16 10:06:29 +0930

    add ProgramAdmin class to manage Program model in admin interface

--- youthjustice_app/admin.py
+++ youthjustice_app/admin.py
@@ -9,3 +9,23 @@ class OrganisationAdmin(admin.ModelAdmin):
     list_display = ("name", "organisation_type", "email", "phone", "created_at")
     search_fields = ("name", "email", "phone")
     list_filter = ("organisation_type",)
+
+
+
+# PROGRAM ADMIN
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "organisation",
+        "region",
+        "category",
+        "is_available",
+        "is_featured",
+        "created_at",
+    )
+    search_fields = ("name", "organisation__name", "short_description")
+    list_filter = ("region", "category", "is_available", "is_featured")
+    list_select_related = ("organisation",)
+
