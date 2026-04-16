02632d22ac73a0f88f8ab9c87608458ce746e4ce
M-Ahmad-T <ch.muhammad.ahmad@gmail.com>
2026-04-16 10:05:57 +0930

    refactor admin registration for Organisation model and remove unused admin classes

--- youthjustice_app/admin.py
+++ youthjustice_app/admin.py
@@ -1,33 +1,11 @@
 from django.contrib import admin
 from .models import Organisation, Program, CrimeDataSnapshot, CrimeData, EngagementData
 
-# Models are being registered here
 
+# ORGANISATION ADMIN
 
-class OrganisationProfileAdmin(admin.ModelAdmin):
-    list_display = ("organisation_name", "contact_email", "region", "user")
-    search_fields = ("organisation_name", "contact_email", "user__username")
-    list_filter = ("region",)
-
-
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ("name", "region", "category", "is_available")
-    list_filter = ("region", "category")
-    search_fields = ("name", "organisation")
-
-admin.site.register(Program, ProgramAdmin)
-
-
-class CrimeDataAdmin(admin.ModelAdmin):
-    list_display = ("region", "offence_category", "year", "month", "count")
-    list_filter = ("region", "year", "offence_category")
-    search_fields = ("offence_category", "offence_type", "region")
-
-admin.site.register(CrimeData, CrimeDataAdmin)
-
-
-class EngagementDataAdmin(admin.ModelAdmin):
-    list_display = ("year", "sex", "indigenous_status", "value_nt", "value_national")
-    list_filter = ("year", "sex", "indigenous_status")
-
-admin.site.register(EngagementData, EngagementDataAdmin)
+@admin.register(Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ("name", "organisation_type", "email", "phone", "created_at")
+    search_fields = ("name", "email", "phone")
+    list_filter = ("organisation_type",)
